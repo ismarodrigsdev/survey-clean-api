4aa1447b8394bc0c154571a51f6f27415656b5e1
Ismael Rodrigues <ismaelf3074@gmail.com>
2023-05-17 17:49:22 -0300

    test: ensure SignUpController throws 500 if email validation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,6 +2,7 @@ import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { EmailValidator } from '../protocols/email-validator'
+import { ServerError } from '../errors/server-error'
 
 interface SutTypes {
   sut: SignUpController
@@ -103,5 +104,26 @@ describe('SignUp Controller', () => {
     }
     sut.handle(httpRequest)
     expect(isValidSpy).toHaveBeenCalledWith('email@mail.com')
+  })
+  test('Should return 500 if emai validator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        throw new Error()
+      }
+    }
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new SignUpController(emailValidatorStub)
+
+    const httpRequest = {
+      body: {
+        name: 'XXXXXXXXXXXXXXXXXX',
+        email: 'invalid_email@mail.com',
+        password: 'XXXXXXXXXXXXXXXXXX',
+        passwordConfirmation: 'XXXXXXXXXXXXXXXXXX'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
