9c6df88b2644e7340e99dbabdb6f4834cfa16197
Ismael Rodrigues <ismaelf3074@gmail.com>
2023-05-18 11:22:13 -0300

    refactor: transform handle to async

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,4 +1,4 @@
 import { HttpResponse, HttpRequest } from '../protocols'
 export interface Controller {
-  handle: (httpRequest: HttpRequest) => HttpResponse
+  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
 }
